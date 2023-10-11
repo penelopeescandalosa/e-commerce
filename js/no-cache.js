5821254d8a42ae70fb6bbbcae7eb0235e4ca50df
Sandro Oliveira <sandrooliveirarm@gmail.com>
2023-10-11 18:45:02 -0300

    copiando arquivo no-cache.js

--- js/no-cache.js
+++ js/no-cache.js
@@ -1,16 +1,24 @@
 //resolve o problema dentro das paginas dos produtos que não da para acessar o jQuery pelo $
-(typeof $.fn == 'undefined') ? $ = jQuery.noConflict() : null;
+var $ = jQuery.noConflict();
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//url base dos arquivos/////////////////////////////////////////////////////////////////////////////////////////////
+const URL_RAIZ = 'https://cdn.jsdelivr.net/gh/penelopeescandalosa/e-commerce@a28c604/';// JSDELIVR
+//const URL_RAIZ = 'https://images.tcdn.com.br/files/805466/themes/143/';// TRAY
 
 $( document ).ready(function() {
-    //$("body").append(     $("<script>").attr("src", "https://images.tcdn.com.br/files/805466/themes/143/js/gerenciador_novo.js?" + rand() )     );//sem cache
-    $("body").append(     $("<script>").attr("src", "https://cdn.jsdelivr.net/gh/penelopeescandalosa/e-commerce@9110652/js/gerenciador_novo.js" )     );//com cache
+
+    //evita alguns erros específicos que estão substituindo o $
+    $ = jQuery.noConflict();
+
+    //inclui o gerenciador no DOM
+    $("body").append(
+        $("<script>").attr("src", URL_RAIZ + "js/gerenciador.js" /* + "?" + rand() */ )
+    );
 });
 
 //gera um numero randomico para contornar caches
 function rand() {
-    const agora = new Date();
-    return Math.floor(agora.getTime() * Math.random());
+    return Math.floor(( new Date() ).getTime() * Math.random());
 }
